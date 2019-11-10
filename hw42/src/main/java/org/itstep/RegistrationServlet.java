17c266183e0c9ffcf78c89fc2ecaeed291f72436
Valeria Lebedeva <frytgy22@rambler.ru>
2019-11-10 19:53:53 +0200

    added hw42

---
+++ hw42/src/main/java/org/itstep/RegistrationServlet.java
@@ -0,0 +1,39 @@
+package org.itstep;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.List;
+
+public class RegistrationServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String login = req.getParameter("login");
+        final String password = req.getParameter("password");
+        final String gender = req.getParameter("gender");
+        final String phone = req.getParameter("phone");
+        final String email = req.getParameter("email");
+        final String sub = req.getParameter("sub");
+
+        User user = new User(login, password, gender, phone, email, sub);
+
+        RegisteredUsers registeredUsers = RegisteredUsers.getInstance();
+
+        File file = new File("/home/valeria/Desktop/registration.txt");
+        List<User> users = registeredUsers.list(file);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        registeredUsers.add(user);
+
+        try (OutputStream outputStream = new FileOutputStream(file)) {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(users);
+        }
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello, " + user.getLogin());
+    }
+}
