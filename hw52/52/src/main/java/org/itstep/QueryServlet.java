552f751608f8c0a8e83ace95e93144a5bee7e131
Maxim <maxshaptala@gmail.com>
2020-01-08 03:29:05 +0200

    добавлены замечания в код

--- hw52/52/src/main/java/org/itstep/QueryServlet.java
+++ hw52/52/src/main/java/org/itstep/QueryServlet.java
@@ -10,20 +10,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
-@WebServlet(
-        name = "registration",
-        urlPatterns = "/registration"
-)
-@WebFilter(filterName = "FilterCharset", urlPatterns = {"/*"})
+@WebServlet(name = "registration", urlPatterns = "/registration")
+@WebFilter(filterName = "FilterCharset", urlPatterns = { "/*" })
 public class QueryServlet extends HttpServlet implements Filter {
 
-    //длинный, но единственный способ который нашла, чтоб вывести русский текст
+    // длинный, но единственный способ который нашла, чтоб вывести русский текст
+    // то же самое можно было сделать в самом сервлете задавая кодировку для объекта
+    // HttpServletRequest и HttpServletResponse
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
         servletRequest.setCharacterEncoding("UTF-8");
         servletResponse.setCharacterEncoding("UTF-8");
         filterChain.doFilter(servletRequest, servletResponse);
@@ -35,14 +36,20 @@ public class QueryServlet extends HttpServlet implements Filter {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // если параметр query не будет задан, то будет исключение NullPointerException
+        // лучше вначале проверять на null, а потом делать trim()
         final String QUERY = req.getParameter("query").trim();
         PrintWriter writer = resp.getWriter();
 
-        String[] movies = {"Movies.MovieId", " Movies.DirectorId", "Movies.Title", "Movies.ReleaseYear", "Movies.Rating", "Movies.Plot", "Movies.MovieLength"};
-        String[] directors = {"Directors.DirectorId", "Directors.FirstName", "Directors.LastName", "Directors.Nationality", "Directors.Birth"};
-        String[] actors = {"Actors.DirectorId", "Actors.FirstName", "Actors.LastName", "Actors.Nationality", "Actors.Birth"};
+        String[] movies = { "Movies.MovieId", " Movies.DirectorId", "Movies.Title", "Movies.ReleaseYear",
+                "Movies.Rating", "Movies.Plot", "Movies.MovieLength" };
+        String[] directors = { "Directors.DirectorId", "Directors.FirstName", "Directors.LastName",
+                "Directors.Nationality", "Directors.Birth" };
+        String[] actors = { "Actors.DirectorId", "Actors.FirstName", "Actors.LastName", "Actors.Nationality",
+                "Actors.Birth" };
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // это нужно делать только один раз. лучше в статическом блоке
+                                                       // инициализации
             String connString = "jdbc:mysql://localhost/MovieBase?serverTimezone=Europe/Kiev&characterEncoding=utf8";
             String user = "root";
             String password = "";
@@ -50,20 +57,19 @@ public class QueryServlet extends HttpServlet implements Filter {
             try (Connection connection = DriverManager.getConnection(connString, user, password)) {
                 Statement statement = connection.createStatement();
 
-                writer.println("<!DOCTYPE html>\n" +
-                        "<html lang=\"en\">\n" +
-                        "<head>\n" +
-                        "    <meta charset=\"UTF-8\">\n" +
-                        "    <title>MySql</title>\n" +
-                        "<link rel=\"stylesheet\" href=\"../css/index.css\">" +
-                        "</head>\n" +
-                        "<body>");
+                writer.println("<!DOCTYPE html>\n" + "<html lang=\"en\">\n" + "<head>\n"
+                        + "    <meta charset=\"UTF-8\">\n" + "    <title>MySql</title>\n"
+                        + "<link rel=\"stylesheet\" href=\"../css/index.css\">" + "</head>\n" + "<body>");
 
-                if (QUERY.length() > 12 && "select".equalsIgnoreCase(QUERY.substring(0, 6)) && QUERY.toUpperCase().contains(" FROM ")) {
-                    ResultSet resultSet = statement.executeQuery(QUERY);
+                if (QUERY.length() > 12 && "select".equalsIgnoreCase(QUERY.substring(0, 6))
+                        && QUERY.toUpperCase().contains(" FROM ")) {
+                    ResultSet resultSet = statement.executeQuery(QUERY); // очень плохая идея выполнять запрос введенный
+                                                                         // пользователем. Это sql инъекция!!!
                     writer.println("<table><thead><tr>\n");
 
-                    String getNameQuery = QUERY.substring(6, QUERY.toUpperCase().indexOf("FROM"));//получаю строку после SELECT до FROM
+                    String getNameQuery = QUERY.substring(6, QUERY.toUpperCase().indexOf("FROM"));// получаю строку
+                                                                                                  // после SELECT до
+                                                                                                  // FROM
                     String[] createTable = getNameQuery.trim().split(",");
 
                     if (getNameQuery.contains("*")) {
@@ -79,9 +85,7 @@ public class QueryServlet extends HttpServlet implements Filter {
                         writer.println("<th>" + s.trim() + "</th>\n");
                     }
 
-                    writer.println("</tr>" +
-                            "</thead><tbody>\n" +
-                            "<tr>\n");
+                    writer.println("</tr>" + "</thead><tbody>\n" + "<tr>\n");
 
                     while (resultSet.next()) {
                         for (String s : createTable) {
